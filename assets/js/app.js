ad2c5f46a70754d45231786cd62242f493b2eae4
Taylor Emolo <tayloremolo@tay.local>
2017-01-19 15:24:05 -0500

    Websignia files

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+	  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+	
+});
